78cd8dca995839595ba1f9224d03c0d8f96fdb56
KPKR1997 <krishnaprakash997@gmail.com>
2025-01-20 22:20:53 +0530

    Update

--- src/exception.py
+++ src/exception.py
@@ -2,7 +2,6 @@ import sys
 from src.logger import logging
 
 
-
 def error_message_detail(error, error_detail: sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
@@ -10,8 +9,6 @@ def error_message_detail(error, error_detail: sys):
         file_name, exc_tb.tb_lineno, str(error))
     return error_message
     
-
-
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
