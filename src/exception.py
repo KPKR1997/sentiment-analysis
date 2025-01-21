fcf39ca3f70a6c3a0f473808a6eda1bb89768e44
KPKR1997 <krishnaprakash997@gmail.com>
2025-01-21 22:56:21 +0530

    DataIngestion

--- src/exception.py
+++ src/exception.py
@@ -2,7 +2,7 @@ import sys
 from src.logger import logging
 
 
-def error_message_detail(error, error_detail: sys):
+def error_message_detail(error, error_detail : sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = 'Error occured in name [{0}] line [{1}] error message [{2}]'.format(
@@ -17,7 +17,6 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-        
+    
 
-        
 
